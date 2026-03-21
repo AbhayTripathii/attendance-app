b7689bc565eeae76bfbfbde6aa62621ebcd060e0
Abhay Tripathii <abhaycoder7838@gmail.com>
2026-03-22 00:23:54 +0530

    fix database url connection

--- config/database.js
+++ config/database.js
@@ -1,19 +1,23 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT || "3306"),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "railway",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  charset: "utf8mb4",
-  timezone: "+00:00",
-  ssl: { rejectUnauthorized: false }
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = mysql.createPool(process.env.DATABASE_URL + "?ssl={'rejectUnauthorized':false}");
+} else {
+  pool = mysql.createPool({
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "railway",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: { rejectUnauthorized: false }
+  });
+}
 
 pool
   .getConnection()
